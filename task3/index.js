303e9a981de45a4d2b7ba5626fe3896df22c0980
Meral7 <113798082+Meral7@users.noreply.github.com>
2024-06-03 09:51:35 +0300

    lab 2

--- task3/index.js
+++ task3/index.js
@@ -1,14 +1,20 @@
 function sortArray() {
     let arr = [];
     let number = 0;
+    document.write("ur values  is ")
     for (let i = 0; i < 5; i++) {
       number = parseInt(prompt(`Enter element number ${i + 1}: `));
+     
+      document.write(number)
       arr.push(number);
     }
     arr.sort((a, b) => a - b);
+    document.write(`<br>`);
+    document.write("ur values  after being sorted descending ")
     document.write(arr);
     arr.reverse();
     document.write(`<br>`);
+    document.write("ur values  after being sorted ascending ")
     document.write(arr);
   }
   sortArray();
